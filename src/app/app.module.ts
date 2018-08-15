d1c65a05ff803a50459614bd379cc4bc26657449
maroom1 <godugulurigmk@gmail.com>
2018-08-15 10:29:33 -0700

    demo providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { PersonComponent } from './components/person/person.component';
 import { PersonListComponent } from './components/person-list/person-list.component';
 import { AddPersonComponent } from './components/add-person/add-person.component';
+import { PersonService } from './services/person.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { AddPersonComponent } from './components/add-person/add-person.component
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PersonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
